24d0381c096ec0fa80e8e26cc5d4365fddfc8d5c
Sungho Park <siminsungho@gmail.com>
2021-10-15 01:28:35 +0900

    update ubinos build scripts

--- config/jansson.cmake
+++ config/jansson.cmake
@@ -1,4 +1,5 @@
 set(INCLUDE__JANSSON TRUE)
+set(PROJECT_UBINOS_LIBRARIES ${PROJECT_UBINOS_LIBRARIES} jansson_wrapper)
 
 set_cache_default(JANSSON__BASE_DIR "${PROJECT_LIBRARY_DIR}/jansson" STRING "jansson project base dir")
 
